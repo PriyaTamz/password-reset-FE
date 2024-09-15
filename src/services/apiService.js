5a0d0a9599b9d14b32cd3275896984488aec2951
vishnupriyatk1996@gmail.com <vishnupriyatk1996@gmail.com>
2024-09-15 14:55:11 +0530

    include api url

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -2,7 +2,7 @@
 const apiUrl = process.env.REACT_APP_API_URL;
 
 export const fetchData = () => {
-  return fetch(`${apiUrl}/some-endpoint`)
+  return fetch(`${apiUrl}/users`)
     .then(response => response.json())
     .catch(error => console.error('Error:', error));
 };
