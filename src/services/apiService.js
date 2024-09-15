022dfe08146e9b7cd8b0859c6e5c9e29d4ea5eaf
vishnupriyatk1996@gmail.com <vishnupriyatk1996@gmail.com>
2024-09-15 15:05:31 +0530

    include api url

--- src/services/apiService.js
+++
@@ -1,8 +0,0 @@
-
-const apiUrl = process.env.REACT_APP_API_URL;
-
-export const fetchData = () => {
-  return fetch(`${apiUrl}/users`)
-    .then(response => response.json())
-    .catch(error => console.error('Error:', error));
-};
--- src/services/instance.js
+++ src/services/instance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseurl = 'http://localhost:3001';
+const baseurl = process.env.REACT_APP_API_URL;
 
 const instance = axios.create({
     baseURL: baseurl,
