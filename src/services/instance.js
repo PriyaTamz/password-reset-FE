25f1cfb3eab1d9d9113b4babe5f4408b6337864e
vishnupriyatk1996@gmail.com <vishnupriyatk1996@gmail.com>
2024-09-15 15:09:50 +0530

    include api-url

--- src/services/instance.js
+++ src/services/instance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseurl = process.env.REACT_APP_API_URL;
+const baseurl = 'https://password-reset-be-1-exn2.onrender.com';
 
 const instance = axios.create({
     baseURL: baseurl,
